34b07a053339db285cb44266714e0ab91d8a799f
TheMindGold <86631935+TheMindGold@users.noreply.github.com>
2022-01-04 00:02:02 +0000

    Add files via upload

---
+++ Discord Bot #2/slashCMD.js
@@ -0,0 +1,29 @@
+const { client } = require(".");
+const config = require("./config.json");
+
+module.exports = async (client) => {
+    const CMD = [
+        {
+            name: 'ping',
+            description: 'I wonder what this does'
+        },
+        {
+            name: 'reaction-role-add',
+            description: 'Reaction role using buttons',
+            options: [{
+                name: 'msg',
+                description: 'Message that is sent',
+                type: 'STRING',
+                required: true
+            },
+            {
+                name: 'role',
+                description: 'The role to be added or removed',
+                type: 'ROLE',
+                required: true
+            }]
+        }
+    ]
+    await client.guilds.cache.get('926852429738278933')?.commands.set(CMD);
+    //await client.application?.commands.set(CMD);
+}
