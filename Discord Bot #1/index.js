dac50abb68555aea547a0e0cdef6347549bcedda
TheMindGold <86631935+TheMindGold@users.noreply.github.com>
2022-01-04 00:00:39 +0000

    Update index.js

--- Discord Bot #1/index.js
+++ Discord Bot #1/index.js
@@ -37,8 +37,6 @@ module.exports.Discord = Discord
 module.exports.config = config
 module.exports.SlashCmd = SlashCmd
 
-require('./mongo.js')()
-
 // Command Handler
 fs.readdirSync('./commands').forEach(dir => {
     fs.readdir(`./commands/${dir}`, (err, files) => {
